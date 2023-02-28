c114e8c2791ce1829233e06430d4f7c5e18c6e1c
Aarys <aaryswastaken@gmail.com>
2023-02-28 06:57:22 +0100

    Forgot to push

---
+++ newuser.js
@@ -0,0 +1,63 @@
+import { readFileSync, writeFileSync, existsSync } from "fs";
+import { hash } from 'bcrypt';
+
+function init_db() {
+    writeFileSync("./databse.json", "{\"users\":[], \"countdowns\": []}");
+}
+
+function import_db() {
+    return JSON.parse(readFileSync("./database.json"));
+}
+
+function save_db() {
+    writeFileSync("./database.json", JSON.stringify(database))
+}
+
+if (!existsSync("./database.json")) {
+    init_db()
+}
+
+let database = import_db();
+
+if (!Object.keys(database).includes("users")) {
+    database.users = [];
+    save_db();
+}
+
+function show_help() {
+    console.log("  newuser.js <username> <password> <display name>");
+    console.log(" -h for help")
+}
+
+if (process.argv.includes("-h") || process.argv.includes("--help") || process.argv.length < 5) {
+    show_help();
+    process.exit(1);
+}
+
+let username = process.argv[2];
+let pwd = process.argv[3];
+let display = process.argv[4];
+
+console.log("**** CREATING NEW USER ****");
+console.log(" Username: "+username);
+console.log(" Display name: "+display);
+console.log(" Password length: "+pwd.length);
+
+if (database.users.map(u => u.username).includes(username)) {
+    console.log("\nUser already exists... Exiting");
+    process.exit(1);
+}
+
+console.log("\nComputing hash ...");
+
+let computed_hash = await hash(pwd, 10);
+
+console.log("[*] Hash complete");
+
+database.users.push({username, display_name: display, hash: computed_hash});
+
+console.log("[!] Saving database...");
+save_db();
+console.log("[#] Database saved...");
+
+console.log("*** User created ***");
---
+++ server.js
@@ -0,0 +1,171 @@
+import fastify from 'fastify';
+import view from '@fastify/view';
+import ejs from 'ejs';
+import jwt from '@fastify/jwt';
+import dotenv from 'dotenv';
+import { compare } from 'bcrypt';
+import SimpleFormPlugin from 'fastify-simple-form';
+import cookie from '@fastify/cookie';
+
+dotenv.config();
+
+import { readFileSync, writeFileSync, existsSync } from "fs";
+
+const dev = process.env.PROD != "true";
+
+const server = fastify();
+
+server.register(view, {
+    engine: {
+        ejs: ejs,
+    },
+});
+
+server.register(jwt, {
+    secret: process.env.APP_SECRET ?? 'thisisasecret',
+    cookie: {
+        cookieName: 'jwt',
+    },
+})
+
+server.register(SimpleFormPlugin, {
+    multipart: true,
+    urlencoded: false,
+});
+
+server.register(cookie, {
+    secret: process.env.COOKIE_SECRET ?? "cookie_secret",
+    hook: 'onRequest',
+    parseOptions: {}
+});
+
+const ressource_whitelist = [ "main.css" ];
+const types = {"css": "text/css"};
+
+function init_db() {
+    writeFileSync("./databse.json", "{\"users\":[], \"countdowns\": []}");
+}
+
+function import_db() {
+    return JSON.parse(readFileSync("./database.json"));
+}
+
+function save_db() {
+    writeFileSync("./database.json", JSON.stringify(database))
+}
+
+if (!existsSync("./database.json")) {
+    init_db()
+}
+
+let database = {};
+let cache = {};
+
+try {
+    database = import_db();
+} catch {
+    init_db();
+    database = import_db();
+}
+
+if (!Object.keys(database).includes("users")) {
+    database.users = [];
+    save_db();
+}
+
+if (!Object.keys(database).includes("countdowns")) {
+    database.countdowns = [];
+    save_db();
+}
+
+server.get("/", async (req, res) => {
+    if (req.cookies["jwt"] === undefined) {
+        res.redirect("/login");
+    } else {
+        if (await req.jwtVerify({onlyCookie: true})) {
+            let decoded = server.jwt.decode(req.cookies["jwt"]);
+            console.log(decoded);
+            res.view("/static/index.ejs", { user: decoded });
+        } else {
+            res.redirect("/login?jwt_e");
+        }
+    }
+})
+
+server.get("/static/:ressource_name", (req, res) => {
+    const { ressource_name } = req.params;
+
+    if (ressource_whitelist.includes(ressource_name)) {
+        let s = ressource_name.split(".");
+        if (Object.keys(types).includes(s[s.length - 1])) {
+            res.header("Content-type", types[s[s.length-1]])
+        }
+        res.send(readFileSync("./static/"+ressource_name));
+    } else {
+        res.code(401);
+        res.send("Unauthorized");
+    }
+})
+
+server.get("/api/countdowns", async (req, res) => {
+    if (req.cookies["jwt"] === undefined) {
+        res.redirect("/login");
+    } else {
+        if (await req.jwtVerify({onlyCookie: true})) {
+            let decoded = server.jwt.decode(req.cookies["jwt"]);
+            
+            res.send(database.countdowns.filter(c => c.user == decoded.username));
+        } else {
+            res.redirect("/login?jwt_e");
+        }
+    }
+})
+
+server.post("/authenticate", {
+    schema: {
+        body: {
+            type: 'object',
+            properties: {
+                username: {
+                    type: 'string',
+                },
+                password: {
+                    type: 'string',
+                },
+                redirect_uri: {
+                    type: 'string',
+                }
+            },
+            required: ['username', 'password'],
+        },
+    }},
+    async (req, res) => {
+        let { username, password, redirect_uri } = req.body;
+
+        if (database.users.map(u => u.username).includes(username)) {
+            let user = database.users.filter(u => u.username == username)[0];
+            let match = await compare(password, user.hash);
+
+            if (match) {
+                const token = await res.jwtSign({ username: user.username, display_name: user.display_name})
+                res.setCookie("jwt", token).redirect(redirect_uri ?? "/");
+            } else {
+                res.redirect("/login?pwd_e");
+            }
+        } else {
+            res.redirect("/login?u_e");
+        }
+    }
+);
+
+server.get("/logout", (req, res) => {
+    res.clearCookie("jwt");
+})
+
+server.listen({ port: dev ? 8080:80 }, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
+})
